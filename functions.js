9985cb9d8f7f9613c42393b008ba8bde8339f6ba
Admin <admin@Admins-MacBook-Pro.local>
2020-12-07 21:20:36 +0200

    simplified using arrow fn + arror fn without phar.

--- functions.js
+++ functions.js
@@ -27,7 +27,7 @@ function showPage(pageId){
 }
 
 function initMenu(){
-    document.addEventListener("click", function(e){
+    document.addEventListener("click", e => {
         const link = e.target;
         if (link.matches("#top-menu-bar a")) {
           const id = link.getAttribute("data-page");
@@ -40,7 +40,7 @@ initMenu();
 showPage(activePage);
 
 function getHTMLSkills(skills) {
-    return skills.map(function(skill){
+    return skills.map(skill => {
     return `<li class= "${skill.endorsements > 9 ? "favorite": ""}"> 
        ${skill.name} <span>&middot; ${skill.endorsements} </span>
     </li>`;
@@ -51,10 +51,10 @@ function showSkills(skills) {
     ul.innerHTML = getHTMLSkills(skills);
 }
 
-fetch("data/skills.json").then(function(r){
+fetch("data/skills.json").then(r => {
    return r.json();
-}).then(function(allskills) {
-    allskills.sort(function(s1, s2) {
+}).then((allskills) => {
+    allskills.sort((s1, s2) => {
     return s2.endorsements - s1.endorsements;
   
 });
